2280a37cbf6713eaaad25f7f4a641f8737a7d9b3
howecofe <am0229@naver.com>
2024-09-12 16:58:47 +0900

    refactor: GlobalExceptionHandler RestController를 ControllerAdvice로 변경

--- src/main/java/org/example/market/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/market/exception/GlobalExceptionHandler.java
@@ -3,13 +3,12 @@ package org.example.market.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UnauthorizedException.class)
