759806f25ed885e144ad0d20347e164aa36ba18c
JUNHA CHOI <choijh0309@naver.com>
2024-09-13 13:41:56 +0900

    - Feat Product Entity 수정

--- src/main/java/org/example/market/domain/Product.java
+++ src/main/java/org/example/market/domain/Product.java
@@ -1,14 +1,10 @@
 package org.example.market.domain;
-
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
-
-import static org.example.market.domain.Product.ProductStatus.*;
-
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,52 +15,41 @@ public class Product {
     private Long id;
     private String name;
     private Long price;
-
     @Enumerated(EnumType.STRING)
-    private ProductStatus status; // "판매중", "예약중", "완료"
-
+    private ProductStatus status;
     @Getter
     public enum ProductStatus {
         FOR_SALE("판매중"),
         RESERVED("예약중"),
         SOLD_OUT("완료");
-
         private final String description;
-
         ProductStatus(String description) {
             this.description = description;
         }
     }
-
     @ManyToOne
     @JoinColumn(name="seller_id")
     private Member seller;
-
     @Column(nullable = false)
     @ColumnDefault("0")
     private int stock;
-
     @Builder
-    public Product(String name, Long price, ProductStatus status, Member seller,int stock) {
+    public Product(String name, Long price, ProductStatus status, Member seller, int stock) {
         this.name = name;
         this.price = price;
         this.status = status;
         this.seller = seller;
         this.stock = stock;
     }
-
-    @Builder
-    public Product(Long id, String name, Long price, ProductStatus status, Member seller,int stock) {
-        this.id = id;
+    public void updateDetails(String name, Long price, int stock) {
         this.name = name;
         this.price = price;
-        this.status = status;
-        this.seller = seller;
-        this.stock=stock;
+        this.stock = stock;
     }
-
-
-    public void soldOut(){
-        this.status= SOLD_OUT;
+    public void setStatus(ProductStatus status) {
+        this.status = status;
+    }
+    public void soldOut() {
+        this.status = ProductStatus.SOLD_OUT;
     }
 }
